5d9804aa551fc7b6b9d7c72a6e25fff147d8aa81
Packata681056 <ribovr@gmail.com>
2024-04-26 09:33:47 +0300

    Rename the form to a more proper name and get values

--- CodeGame/CodeGame/SignUpForm.Designer.cs
+++ CodeGame/CodeGame/SignUpForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CodeGame
 {
-    partial class Form2
+    partial class SignUpForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            label2 = new Label();
+            label1 = new Label();
+            txtBoxPassword = new TextBox();
+            txtBoxUsername = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(270, 178);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Username";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(270, 232);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Password";
+            // 
+            // txtBoxPassword
+            // 
+            txtBoxPassword.Location = new Point(270, 250);
+            txtBoxPassword.Name = "txtBoxPassword";
+            txtBoxPassword.PasswordChar = '*';
+            txtBoxPassword.Size = new Size(261, 23);
+            txtBoxPassword.TabIndex = 5;
+            // 
+            // txtBoxUsername
+            // 
+            txtBoxUsername.Location = new Point(270, 196);
+            txtBoxUsername.Name = "txtBoxUsername";
+            txtBoxUsername.Size = new Size(261, 23);
+            txtBoxUsername.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(352, 279);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 23);
+            button1.TabIndex = 8;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // SignUpForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtBoxPassword);
+            Controls.Add(txtBoxUsername);
+            Name = "SignUpForm";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label2;
+        private Label label1;
+        private TextBox txtBoxPassword;
+        private TextBox txtBoxUsername;
+        private Button button1;
     }
 }
--- CodeGame/CodeGame/SignUpForm.cs
+++ CodeGame/CodeGame/SignUpForm.cs
@@ -10,11 +10,19 @@ using System.Windows.Forms;
 
 namespace CodeGame
 {
-    public partial class Form2 : Form
+    public partial class SignUpForm : Form
     {
-        public Form2()
+        public SignUpForm()
         {
             InitializeComponent();
+        }
+
+        // Register Function that will accept username and password and register into the game and directly login in 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Getting the username and password from the textboxes
+            string username = txtBoxUsername.Text;
+            string password = txtBoxPassword.Text;
         }
     }
 }
